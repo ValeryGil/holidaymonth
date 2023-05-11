a4aff58f35ee5540f0eda27339d903341387e920
ValeryGil <valery.gil@icloud.com>
2023-05-11 15:46:02 +0300

    First commit

---
+++ src/test/java/VacationServiceTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VacationServiceTest {
+
+    @Test
+    void emulationOne() {
+        VacationService service = new VacationService();
+
+        // подготавливаем данные:
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int expected = 3;
+
+        // вызываем целевой метод:
+        int actual = service.calculate(income, expenses, threshold);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void emulationTwo() {
+        VacationService service = new VacationService();
+
+        // подготавливаем данные:
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        // вызываем целевой метод:
+        int actual = service.calculate(income, expenses, threshold);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+}
